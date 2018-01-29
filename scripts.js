8fb6caced2f35077d29dd6dc5683953e7e0d82ff
Paweł Gardziel <gardzielpawel@gmail.com>
2018-01-29 11:08:36 +0100

    moduł 11.5

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
-function Phone(brand, price, color) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".")
+function Button (text) {
+	this.text = 'Hello';
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var Samsung = new Phone("Samsung", 1700, "black");
-var OnePlus = new Phone("OnePlus One", 1500, "green");
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		this.$element.appendTo($('body'));
+	}
+};
 
-iPhone6S.printInfo();
-Samsung.printInfo();
-OnePlus.printInfo();
+var btn1 = new Button('Hello!');
+
+
+btn1.create();
